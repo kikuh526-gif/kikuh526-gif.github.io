4f4312d9bbf9276678a62aead400e10dd34cf6f3
Haruki Kikuchi <kikuh526@gmail.com>
2026-03-23 00:36:18 +0900

    Quartz sync: Mar 23, 2026, 12:36 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "論文置き場",
+    pageTitle: "保管庫",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
